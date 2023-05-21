332a6b23e3c226b5d439dd2b18dd91d517d74234
Budiman Arbenta <budiman.arbenta@ui.ac.id>
2023-05-21 22:24:43 +0700

    feat(users): add program_studi field to User

--- sipeta_backend/users/models.py
+++ sipeta_backend/users/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import BooleanField, CharField, UUIDField
 from django.utils.translation import gettext_lazy as _
 
+from sipeta_backend.users.constants import PRODI_CHOICES
+
 
 class User(AbstractUser):
     """
@@ -20,6 +22,9 @@ class User(AbstractUser):
     last_name = None  # type: ignore
 
     kode_identitas = CharField(_("Kode Identitas"), blank=True, max_length=255)
+    program_studi = CharField(
+        _("Program Studi"), blank=True, max_length=60, choices=PRODI_CHOICES
+    )
     role_pengguna = CharField(_("Role Pengguna"), blank=True, max_length=255)
     is_dosen_ta = BooleanField(_("Dosen TA"), blank=False, default=False)
     is_dosen_eksternal = BooleanField(_("Dosen Eksternal"), blank=False, default=False)
