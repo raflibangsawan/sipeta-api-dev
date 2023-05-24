f36b1dc8f3e11da5ac0c741403ce98fa1ca5883a
Budiman Arbenta <budiman.arbenta@ui.ac.id>
2023-05-24 22:13:56 +0700

    feat(pengumuman): read detail pengumuman

--- sipeta_backend/pengumuman/urls.py
+++ sipeta_backend/pengumuman/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from sipeta_backend.pengumuman.views import PengumumanView
+from sipeta_backend.pengumuman.views import PengumumanDetailView, PengumumanView
 
 app_name = "pengumuman"
 urlpatterns = [
     path("", view=PengumumanView.as_view()),
+    path("<uuid:id>/", view=PengumumanDetailView.as_view()),
 ]
--- sipeta_backend/pengumuman/views.py
+++ sipeta_backend/pengumuman/views.py
@@ -1,10 +1,17 @@
 from rest_framework import permissions
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 
 from sipeta_backend.pengumuman.models import Pengumuman
 from sipeta_backend.pengumuman.serializers import PengumumanSerializer
 from sipeta_backend.semester.models import Semester
+from sipeta_backend.users.permissions import (
+    IsAdmin,
+    IsDosenTa,
+    IsMethodReadOnly,
+    IsStaffSekre,
+)
 
 
 class PengumumanView(APIView):
@@ -14,4 +21,21 @@ class PengumumanView(APIView):
         current_semester = Semester._get_active_semester()
         pengumumans = Pengumuman.objects.filter(semester=current_semester)
         serializer = PengumumanSerializer(pengumumans, many=True)
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+
+class PengumumanDetailView(APIView):
+    permission_classes = (
+        permissions.IsAuthenticated,
+        IsAdmin | IsStaffSekre | IsDosenTa | IsMethodReadOnly,
+    )
+
+    @property
+    def pengumuman(self):
+        if not hasattr(self, "_pengumuman"):
+            self._pengumuman = Pengumuman.objects.get(id_pengumuman=self.kwargs["id"])
+        return self._pengumuman
+
+    def get(self, request, *args, **kwargs):
+        serializer = PengumumanSerializer(self.pengumuman)
+        return Response(serializer.data, status=HTTP_200_OK)
